8ca794e4976bed0c8e3cafa65b3e0a4b39420235
sauthankar1 <135382159+sauthankar1@users.noreply.github.com>
2023-06-04 19:21:54 +0530

    Update streamlit_app.py
    added snowflake connection lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-import snowflake.connector
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
@@ -34,3 +32,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the result in tabular form
 streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
